5401506fe45ea59be91d715dba2305912490374e
Kuntai Du <kuntai@farewell.cs.uchicago.edu>
2020-08-25 22:42:37 -0500

    finish inference

--- dnn/dnn.py
+++ dnn/dnn.py
@@ -3,12 +3,10 @@ from abc import ABC, abstractmethod
 
 class DNN(ABC):
 
-    # place the model to cpu
     @abstractmethod
     def cpu(self):
         pass
 
-    # place the model to gpu
     @abstractmethod
     def cuda(self):
         pass
@@ -17,6 +15,5 @@ class DNN(ABC):
     def inference(self, video, requires_grad):
         pass
 
-    # get the name of the class
     def name(self):
         return type(self).__name__
