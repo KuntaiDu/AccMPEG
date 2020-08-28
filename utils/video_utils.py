f82e747d9346e3f7d7f73755ab10f7ee59f73c67
Kuntai Du <kuntai@farewell.cs.uchicago.edu>
2020-08-28 15:37:19 -0500

    finalize the framework

--- utils/video_utils.py
+++ utils/video_utils.py
@@ -3,13 +3,13 @@ import torch
 from torchvision import io
 import os
 
-def read_video(video_list, logger):
+def read_videos(video_list, logger):
     '''
         Read a list of video and return two lists. 
         One is the video tensors, the other is the bandwidths.
     '''
-    video_list = [{'video': read_single_video(video_name, logger), 
-                   'bandwidth': read_single_bandwidth(video_name)}
+    video_list = [{'video': read_video(video_name, logger),
+                   'bandwidth': read_bandwidth(video_name)}
                   for video_name in video_list]
     video_list = sorted(video_list, key=lambda x: x['bandwidth'])
 
@@ -20,18 +20,22 @@ def read_video(video_list, logger):
 
     return [i['video'] for i in video_list], [i['bandwidth'] for i in video_list]
 
-def read_single_video(video_name, logger):
+def read_video(video_name, logger):
     logger.info(f'Reading {video_name}')
     if 'mp4' in video_name:
         return io.read_video(video_name, pts_unit='sec')[0].float().div(255).permute(0, 3, 1, 2)
 
-def read_single_bandwidth(video_name):
+def read_bandwidth(video_name):
     if 'mp4' in video_name:
         return os.path.getsize(video_name)
 
-def save_single_video(video_tensor, video_name, logger):
+def write_video(video_tensor, video_name, logger):
 
     logger.info(f'Saving {video_name}')
 
+    # [N, C, H, W] ==> [N, H, W, C]
+    video_tensor = video_tensor.permute(0, 2, 3, 1)
+    # go back to original domain
+    video_tensor = video_tensor.mul(255).add_(0.5).clamp_(0, 255).to('cpu', torch.uint8)
     # lossless encode. Should be replaced
-    io.write_video(video_name, video_tensor, fps=25, options={'crf': 0})
+    io.write_video(video_name, video_tensor, fps=25, options={'crf': '0'})
