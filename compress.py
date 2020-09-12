44fc5722bfa7eb8ceb2f6e6a3d96d39d4c69c53a
Kuntai Du <kuntai@farewell.cs.uchicago.edu>
2020-09-12 00:59:22 -0500

    edit the loss function

--- compress.py
+++ compress.py
@@ -13,6 +13,7 @@ import matplotlib.pyplot as plt
 from dnn.fasterrcnn_resnet50 import FasterRCNN_ResNet50_FPN
 from utils.video_utils import read_videos, write_video, get_qp_from_name
 from utils.mask_utils import *
+from utils.results_utils import read_results
 
 
 def main(args):
@@ -25,9 +26,10 @@ def main(args):
     
     # read the video frames (will use the largest video as ground truth)
     videos, bws, video_names = read_videos(args.inputs, logger, sort=True)
-    videos = [torch.zeros_like(videos[-1])] + videos
-    bws = [0] + bws
-    qps = [-1] + [get_qp_from_name(video_name) for video_name in video_names]
+    videos = videos
+    bws = bws
+    qps = [get_qp_from_name(video_name) for video_name in video_names]
+    
     
     # construct applications
     application_bundle = [FasterRCNN_ResNet50_FPN()]
@@ -46,15 +48,17 @@ def main(args):
 
         optimizer.zero_grad()
         (args.mask_weight * mask.pow(args.mask_p).abs().mean()).backward()
-        (args.cont_weight * (mask[1:, :, :, :] - mask[:-1, :, :, :]).abs().pow(args.cont_p).mean()).backward()
+        if args.cont_weight > 0:
+            (args.cont_weight * (mask[1:, :, :, :] - mask[:-1, :, :, :]).abs().pow(args.cont_p).mean()).backward()
 
         for application in application_bundle:
+            # read ground truth results
+            ground_truth_results = read_results(args.ground_truth, application.name, logger)
 
             logger.info(f'Processing application {application.name}')
             progress_bar = enlighten.get_manager().counter(total=videos[-1].shape[0], desc=f'Iteration {iteration}: {application.name}', unit='frames')
 
             application.cuda()
-            last_mask_slice = None
 
             for fid, (video_slices, mask_slice) in enumerate(zip(zip(*[video.split(1) for video in videos]), mask.split(1))):
 
@@ -65,8 +69,10 @@ def main(args):
                 masked_image = generate_masked_image(mask_slice, video_slices, bws)
 
                 # calculate the loss
-                loss = application.calc_loss(masked_image.cuda(), video_slices[-1].cuda(), args)
-                loss.backward(retain_graph=True)
+                loss = application.calc_diff_acc(masked_image.cuda(), ground_truth_results[fid], args)
+                if loss < 1.0:
+                    loss.backward()
+                    loss = loss.item()
 
                 # visualization
                 if fid % 20 == 0 and iteration % 5 == 4:
@@ -74,37 +80,26 @@ def main(args):
                     plt.clf()
                     ax = sns.heatmap(heat.detach().numpy(), zorder=3, alpha=0.7)
                     ax.imshow(T.ToPILImage()(video_slices[-1][0, :, :, :]), zorder=3, alpha=0.4)
-                    plt.savefig('visualize/%010d-attn.png' % fid, bbox_inches='tight')
+                    plt.savefig(f'visualize/{args.output}_{fid}_attn.png', bbox_inches='tight')
                 
-                # calculate the loss
-                
-                last_mask_slice = mask_slice
 
             application.cpu()
-
-        logger.info('Bef: Mask max: %.3f, min: %.3f, mean: %.3f, std: %.3f' % (mask.max().item(), mask.min().item(), mask.mean().item(), mask.std().item()))
-        
+            
         optimizer.step()
-
-        logger.info('Ste: Mask max: %.3f, min: %.3f, mean: %.3f, std: %.3f' % (mask.max().item(), mask.min().item(), mask.mean().item(), mask.std().item()))
-
-        # clip mask to [0, 1]-range
+        
+        # clip mask
         mask_clip(mask, bws[0])
 
-        logger.info('Aft: Mask max: %.3f, min: %.3f, mean: %.3f, std: %.3f' % (mask.max().item(), mask.min().item(), mask.mean().item(), mask.std().item()))
+        logger.info('Loss: %0.3f, Mask max: %.3f, min: %.3f, mean: %.3f, std: %.3f' % (loss, mask.max().item(), mask.min().item(), mask.mean().item(), mask.std().item()))
 
     # optimization done. No more gradients required.
     mask.requires_grad = False
-    # "binarize" the mask
-    binarize_mask(mask, bws)
-    # generate the compressed video based on the mask
+    # write raw video
     masked_video = generate_masked_video(mask, videos, bws, args)
-    # import pdb; pdb.set_trace()
     write_masked_video(mask, args, qps, bws, logger)
+    # write binarized video
+    binarize_mask(mask, bws)
     write_video(masked_video, args.output, logger)
-    # generate estimated bandwidth
-    logger.info(f'The estimated normalized bandwidth is {mask.mean().item()}')
-
 
 
 if __name__ == '__main__':
@@ -116,14 +111,16 @@ if __name__ == '__main__':
 
     parser.add_argument('-i', '--inputs', nargs = '+', help='The video file names. The largest video file will be the ground truth.', required=True)
     parser.add_argument('-s', '--source', type=str, help='The original video source.', required=True)
+    parser.add_argument('-g', '--ground_truth', type=str, help='The ground truth results.', required=True)
     parser.add_argument('-o', '--output', type=str, help='The output name.', required=True)
     parser.add_argument('--confidence_threshold', type=float, help='The confidence score threshold for calculating accuracy.', default=0.3)
-    parser.add_argument('--num_iterations', type=int, help='Number of iterations for optimizing the mask.', default=20)
+    parser.add_argument('--iou_threshold', type=float, help='The IoU threshold for calculating accuracy in object detection.', default=0.3)
+    parser.add_argument('--num_iterations', type=int, help='Number of iterations for optimizing the mask.', default=30)
     parser.add_argument('--tile_size', type=int, help='The tile size of the mask.', default=16)
-    parser.add_argument('--learning_rate', type=float, help='The learning rate.', default=0.1)
-    parser.add_argument('--mask_weight', type=float, help='The weight of the mask normalization term', default=256)
+    parser.add_argument('--learning_rate', type=float, help='The learning rate.', default=0.07)
+    parser.add_argument('--mask_weight', type=float, help='The weight of the mask normalization term', default=128)
     parser.add_argument('--mask_p', type=int, help='The p-norm for the mask.', default=1)
-    parser.add_argument('--cont_weight', type=float, help='The weight of the continuity normalization term', default=256)
+    parser.add_argument('--cont_weight', type=float, help='The weight of the continuity normalization term', default=0)
     parser.add_argument('--cont_p', type=int, help='The p-norm for the continuity.', default=1)
 
 
