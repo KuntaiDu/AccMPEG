a8c4a21d8aa7ab5453eb2871bfbef275d60194df
Kuntai Du <kuntai@farewell.cs.uchicago.edu>
2020-12-17 23:59:59 -0600

    clean some scripts

--- generate_ground_truth_curve.py
+++ generate_ground_truth_curve.py
@@ -5,7 +5,7 @@ import subprocess
 from pathlib import Path
 
 qp = 30
-quality_list = ["veryfast", "faster", "fast", "medium", "slow", "slower", "veryslow"]
+
 
 # qp_list = [42, 46, 50]
 # qp_list = [34]
@@ -20,42 +20,6 @@ def main(args):
         video_name = Path(video_name)
 
         # os.system(f"rm {video_name}_qp_{qp}_*.mp4")
-
-        output_names = []
-        for quality in quality_list:
-            input_name = f"{video_name}/%010d.png"
-            output_name = f"{video_name}_qp_{qp}_{quality}.mp4"
-            output_names.append(output_name)
-            print(f"Generate video for {output_name}")
-            # encode_with_qp(input_name, output_name, qp, args)
-
-            if not os.path.exists(output_name):
-
-                subprocess.run(
-                    [
-                        "ffmpeg",
-                        "-y",
-                        "-i",
-                        input_name,
-                        "-start_number",
-                        "0",
-                        "-qmin",
-                        f"{qp}",
-                        "-qmax",
-                        f"{qp}",
-                        "-preset",
-                        f"{quality}",
-                        output_name,
-                    ]
-                )
-
-                subprocess.run(["python", "inference.py", "-i", output_name])
-
-        subprocess.run(
-            ["python", "merge_ground_truth.py", "-i"]
-            + output_names
-            + ["-o", f"{video_name}_qp_{qp}_ground_truth.mp4"]
-        )
 
         # for quality in quality_list:
         #     x = f"{video_name}_qp_{qp}_{quality}.mp4"
@@ -75,9 +39,6 @@ if __name__ == "__main__":
         nargs="+",
         help="The video file names. The largest video file will be the ground truth.",
         required=True,
-    )
-    parser.add_argument(
-        "--tile_size", type=int, help="The tile size of the mask.", default=16
     )
     args = parser.parse_args()
     main(args)
