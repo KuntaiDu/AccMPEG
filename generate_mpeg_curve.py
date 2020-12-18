3e80491d632496637bc8290e4a1f14df6819ae77
Kuntai Du <kuntai@farewell.cs.uchicago.edu>
2020-12-17 23:59:59 -0600

    clean some scripts

--- generate_mpeg_curve.py
+++ generate_mpeg_curve.py
@@ -4,11 +4,9 @@ import os
 import subprocess
 from pathlib import Path
 
+gt_qp = 30
 qp_list = [30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50]
-# qp_list = [42, 46, 50]
-# qp_list = [34]
-# qp_list = [20]
-# qp_list = [qp_list[0]]
+quality_list = ["veryfast", "faster", "fast", "medium", "slow", "slower", "veryslow"]
 
 
 def main(args):
@@ -16,6 +14,45 @@ def main(args):
     for video_name in args.inputs:
         assert Path(video_name).is_dir()
         video_name = Path(video_name)
+
+        # generate ground truth
+        output_names = []
+        for quality in quality_list:
+            input_name = f"{video_name}/%010d.png"
+            output_name = f"{video_name}_qp_{gt_qp}_{quality}.mp4"
+            output_names.append(output_name)
+            print(f"Generate video for {output_name}")
+            # encode_with_qp(input_name, output_name, qp, args)
+
+            if not os.path.exists(output_name):
+
+                subprocess.run(
+                    [
+                        "ffmpeg",
+                        "-y",
+                        "-i",
+                        input_name,
+                        "-start_number",
+                        "0",
+                        "-qmin",
+                        f"{gt_qp}",
+                        "-qmax",
+                        f"{gt_qp}",
+                        "-preset",
+                        f"{quality}",
+                        output_name,
+                    ]
+                )
+
+                subprocess.run(["python", "inference.py", "-i", output_name])
+
+        subprocess.run(
+            ["python", "merge_ground_truth.py", "-i"]
+            + output_names
+            + ["-o", f"{video_name}_qp_{gt_qp}_ground_truth.mp4"]
+        )
+
+        # generate mpeg curve
         for qp in qp_list:
             input_name = f"{video_name}/%010d.png"
             output_name = f"{video_name}_qp_{qp}.mp4"
@@ -67,8 +104,6 @@ if __name__ == "__main__":
         help="The video file names. The largest video file will be the ground truth.",
         required=True,
     )
-    parser.add_argument(
-        "--tile_size", type=int, help="The tile size of the mask.", default=16
-    )
+
     args = parser.parse_args()
     main(args)
