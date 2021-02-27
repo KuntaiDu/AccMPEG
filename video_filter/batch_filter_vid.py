5cfd8ef57d6877b3f341d5ad0fb8adadb9c737f2
Alex-q-z <qizhengz@uchicago.edu>
2021-02-27 13:47:04 -0600

    Update latest changes to the filter script

--- video_filter/batch_filter_vid.py
+++ video_filter/batch_filter_vid.py
@@ -1,12 +1,12 @@
 import os
 from itertools import product
 
-video_folder_path = "dataset_original"
-results_folder_path = "dataset_filtered"
-video_list = ["dance_5"]
-#upper_bound_list = [0.15, 0.2, 0.25, 0.3]
-upper_bound_list = [0.25, 0.3]
-lower_bound_list = [0.01]
+video_folder_path = "/tank/qizheng/dds-keypoint/video-filter/dataset"
+results_folder_path = "/tank/qizheng/vid_comp_master/videos"
+video_list = ["surf_4_cropped","surf_5","surf_6","surf_7"]
+upper_bound_list = [0.25]
+lower_bound_list = [0.0025, 0.005, 0.01]
+confidence_threshold = 0.9
 
 for v, ubound, lbound in product(video_list, upper_bound_list, lower_bound_list):
     os.system(
@@ -15,4 +15,5 @@ for v, ubound, lbound in product(video_list, upper_bound_list, lower_bound_list)
         f" --result_folder_path {results_folder_path} "
         f" --stats_file_path stats "
         f" --upper_bound {ubound} --lower_bound {lbound} "
+        f" --confidence_threshold {confidence_threshold} "
     )
