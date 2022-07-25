db73bd2cdb7bdb683d3cf4f727552fbf6e209c30
Kuntai Du <kuntai@farewell.cs.uchicago.edu>
2022-07-24 21:57:23 -0500

    add accmpeg model runtime measurement

---
+++ measurements/benchmark_accmpeg.py
@@ -0,0 +1,62 @@
+import logging
+import os
+
+import coloredlogs
+import numpy as np
+from openvino.inference_engine import Blob, IECore, TensorDesc
+
+from timer import Timer
+
+coloredlogs.install(
+    fmt="%(asctime)s [%(levelname)s] %(name)s:%(funcName)s[%(lineno)s] -- %(message)s",
+    level="INFO",
+)
+
+
+logger = logging.getLogger("AccMPEG")
+
+os.system("rm AccMPEG.log")
+fh = logging.FileHandler("AccMPEG.log")
+logger.addHandler(fh)
+
+
+XML_PATH = "./onnx/accmpeg.xml"
+BIN_PATH = "./onnx/accmpeg.bin"
+ie_core_handler = IECore()
+network = ie_core_handler.read_network(model=XML_PATH, weights=BIN_PATH)
+executable_network = ie_core_handler.load_network(
+    network, device_name="CPU", num_requests=100
+)
+
+random_input_data = np.random.randn(1, 3, 720, 1280).astype(np.float32)
+tensor_description = TensorDesc(
+    precision="FP32", dims=(1, 3, 720, 1280), layout="NCHW"
+)
+input_blob = Blob(tensor_description, random_input_data)
+
+
+for i in range(100):
+
+    with Timer("AccMPEG", logger):
+
+        inference_request = executable_network.requests[i]
+
+        inference_request.set_blob(blob_name="input", blob=input_blob)
+        inference_request.infer()
+        output = inference_request.output_blobs["output"].buffer
+
+
+import re
+
+import numpy as np
+
+pattern = r"([0-1].*)"
+pattern = re.compile(pattern)
+times = []
+
+with open("AccMPEG.log", "r") as f:
+    for val in re.findall(pattern, f.read()):
+        times.append(float(val))
+
+print("AccMPEG runs for ", np.mean(times), " sec per video chunk.")
+
